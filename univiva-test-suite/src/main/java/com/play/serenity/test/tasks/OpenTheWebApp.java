8fd50dceffdc141d83f70912ff170fe18478d1cb
Syamkrishna MG <mgsyamkrishna13@gmail.com>
2020-09-22 12:04:20 +0200

    removed unused files

--- univiva-test-suite/src/main/java/com/play/serenity/test/tasks/OpenTheWebApp.java
+++ univiva-test-suite/src/main/java/com/play/serenity/test/tasks/OpenTheWebApp.java
@@ -4,7 +4,6 @@ import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Open;
 import net.thucydides.core.annotations.Step;
-import com.play.serenity.test.ui.GoogleHomePage;
 import com.play.serenity.test.ui.UnivivaHomePage;
 
 import static net.serenitybdd.screenplay.Tasks.instrumented;
